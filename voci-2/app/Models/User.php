6c0ea73f36aed482b289652581cf98461bc16b87
Braeiker <samueletantillo@gmail.com>
2024-03-04 18:13:09 +0100

    rename

--- voci-2/app/Models/User.php
+++ voci-2/app/Models/User.php
@@ -2,14 +2,13 @@
 
 namespace App\Models;
 
-use App\Models\Deck;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Laravel\Sanctum\HasApiTokens;
-use Illuminate\Notifications\Notifiable;
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -42,8 +41,4 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-
-    public function decks() {
-        return $this->hasMany(Deck::class);
-    }
 }
